175d91bee812038a853b5cd87214efeda2480b1e
Eloy <eloygpdevtest@protonmail.com>
2025-07-06 23:02:43 +0200

    Day 7. Namespaces y models

--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,20 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '50.000 €'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programador',
-                'salary' => '10.000 €'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Profesor',
-                'salary' => '30.000 €'
-            ],
-
-        ]
-    ]);
+    return view('jobs', ['jobs'=>Job::all()]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '50.000 €'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programador',
-            'salary' => '10.000 €'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Profesor',
-            'salary' => '30.000 €'
-        ],
+Route::get('/jobs/{id}', function ($id){    
 
-    ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
