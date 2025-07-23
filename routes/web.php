9104c30add04577673170b8d8de3a60a182b3f07
Eloy <eloygpdevtest@protonmail.com>
2025-07-23 15:29:44 +0200

    Intro a Eloquent

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs'=>Job::all()]);
+    return view('jobs', ['jobs' => Job::all()]);
 });
 
-Route::get('/jobs/{id}', function ($id){    
+Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
     return view('job', ['job' => $job]);
