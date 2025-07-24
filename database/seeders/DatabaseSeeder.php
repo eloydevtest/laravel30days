3bf5b22ae672183ed133d8fc398572b69b4aa22f
Eloy <eloygpdevtest@protonmail.com>
2025-07-24 17:45:25 +0200

    Dia 15. Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Nom',
+            'last_name' => 'Cognom',
             'email' => 'test@example.com',
         ]);
+
+        //Job::factory(200)->create();
+
+        $this->call(JobSeeder::class);
     }
 }
