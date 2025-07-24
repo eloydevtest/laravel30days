5b0ff6a6cf6a0fee3da0ce8040e4973cdfc25173
Eloy <eloygpdevtest@protonmail.com>
2025-07-24 11:27:42 +0200

    Dia 12. Tablas PIVOT

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -13,7 +13,12 @@ class Job extends Model
   protected $table = 'job_listings';
   protected $fillable = ['title', 'salary'];
 
-  public function employer(){
+  public function employer()
+  {
     return $this->belongsTo(Employer::class);
+  }
+
+  public function tags(){
+    return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listings_id');
   }
 }
