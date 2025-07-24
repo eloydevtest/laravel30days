cb821dc7f68930e33a067c8216e4a82d4047c203
Eloy <eloygpdevtest@protonmail.com>
2025-07-24 19:46:52 +0200

    Dia 16. Formularios y CSRF

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,8 +11,9 @@ class Job extends Model
   use HasFactory;
 
   protected $table = 'job_listings';
-  protected $fillable = ['title', 'salary'];
-
+  //protected $fillable = ['employer_id', 'title', 'salary'];
+  protected $guarded = [];
+  
   public function employer()
   {
     return $this->belongsTo(Employer::class);
--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,32 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     //$jobs = Job::with('employer')->paginate(5); //Menos eficiente. Puedes ver el número total de registros.
-    //$jobs = Job::with('employer')->simplePaginate(5); //Más eficiente que el anterior. Puedes acceder a una página en concreto.
-    $jobs = Job::with('employer')->cursorPaginate(5); //Más eficiente que los anteriores. Para muchos registros. No puedes acceder a una página en concreto
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->simplePaginate(5); //Más eficiente que el anterior. Puedes acceder a una página en concreto.
+    //$jobs = Job::with('employer')->cursorPaginate(5); //Más eficiente que los anteriores. Para muchos registros. No puedes acceder a una página en concreto
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
+});
+
+Route::get('/jobs/create', function (){
+    return view('jobs.create');
+});
+
+Route::post('/jobs/', function (){
+    //dd(request()->all());
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
