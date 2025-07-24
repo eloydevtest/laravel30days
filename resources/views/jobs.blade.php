9c3d5db7a2a0bcef142c330ca7bd25e77befa104
Eloy <eloygpdevtest@protonmail.com>
2025-07-24 13:09:01 +0200

    Dia 13. LazyLoading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
     <x-slot:heading>Jobs Listing</x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                    <strong>{{ $job['title'] }}</strong> Sueldo anual:
-                    {{ $job['salary'] }}
-                </a>
-            </li>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-grey-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
+                    <strong>{{ $job['title'] }}</strong> Sueldo anual: {{ $job['salary'] }}
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
