8bfd6d592f2e8c16d9f4fc44bd8d00373bf4f8fd
Eloy <eloygpdevtest@protonmail.com>
2025-07-25 22:58:49 +0200

    Dia 21. Login y registro. Parte 2.

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,14 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+    protected $guarded = [];
+    /*
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
-    ];
+    ];*/  
 
     /**
      * The attributes that should be hidden for serialization.
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,7 +43,7 @@
                     <x-form-field>
                         <x-form-label for="password_confirmation">Confirm password</x-form-label>
                         <div class="mt-2">
-                            <x-form-input id="password_confirmation" name="password_confirmation" type="password_confirmation" required></x-form-input>
+                            <x-form-input id="password_confirmation" name="password_confirmation" type="password" required></x-form-input>
                             <x-form-error name="password_confirmation" />
                         </div>
                     </x-form-field>
