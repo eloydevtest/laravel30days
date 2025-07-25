c752924c47a7852c6812eb4f804a0112fb5c6891
Eloy <eloygpdevtest@protonmail.com>
2025-07-25 17:28:44 +0200

    Dia 20. Login i registro

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
 FROM php:8.3-fpm
 
 RUN apt-get update && apt-get install -y \
-    git curl libpng-dev libonig-dev libxml2-dev zip unzip libzip-dev libicu-dev\
-    && docker-php-ext-install pdo_mysql mbstring zip exif pcntl bcmath gd \
-    && docker-php-ext-install intl
+    git curl libpng-dev libonig-dev libxml2-dev zip unzip libzip-dev libicu-dev \
+    && docker-php-ext-install pdo_mysql mbstring zip exif pcntl bcmath gd intl
 
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+RUN composer global require laravel/installer
+
+RUN git config --global --add safe.directory /var/www
+
+
+# Instalar Node.js
+RUN curl -fsSL https://deb.nodesource.com/setup_20.x | bash - \
+    && apt-get install -y nodejs
+
+ENV PATH="$PATH:/root/.composer/vendor/bin"
 
 WORKDIR /var/www
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,26 @@
 <x-layout>
     <x-slot:heading>
-        Create Job
+        Log in
     </x-slot:heading>
-    <form method="POST" action="/jobs">
+    <form method="POST" action="/login">
         @csrf
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
-                <h2 class="text-base/7 font-semibold text-gray-900">Crea Nuevo Trabajo</h2>
-                <p class="mt-1 text-sm/6 text-gray-600">Solo necesitamos algunos detalles</p>
-
-                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+               
+                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <x-form-field>
-                        <x-form-label for="title">Title</x-form-label>
+                        <x-form-label for="email">Email</x-form-label>
                         <div class="mt-2">
-                            <x-form-input id="title" name="title" placeholder="CEO" required></x-form-input>
-                            <x-form-error name="title" />
+                            <x-form-input id="email" name="email" type="email" required></x-form-input>
+                            <x-form-error name="email" />
                         </div>
                     </x-form-field>
 
                     <x-form-field>
-                        <x-form-label for="salary">Salary</x-form-label>
+                        <x-form-label for="password">Password</x-form-label>
                         <div class="mt-2">
-                            <x-form-input id="salary" name="salary" placeholder="30.000 €" required></x-form-input>
-                            <x-form-error name="salary" />
+                            <x-form-input id="password" name="password" type="password" required></x-form-input>
+                            <x-form-error name="password" />
                         </div>
                     </x-form-field>
 
@@ -43,8 +41,8 @@
 
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
-                <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-                <x-form-button>Save</x-form-button>
+                <a href="/" class="text-sm leading-6 font-semibold text-gray-900">Cancel</button>
+                <x-form-button>Log in</x-form-button>
             </div>
     </form>
 
